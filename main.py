d63451271cf1e13f58cd5a5f1ec98b9c2112e6e8
Danish <muhammadd03@GMAIL.COM>
2023-08-09 11:59:28 +0500

    updated

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import random
 import string
 
-letters = string.ascii_letters
-numbers = string.digits
-symbols = string.punctuation
+# Define sets of characters to choose from
+letters = string.ascii_letters  # Includes both uppercase and lowercase letters
+numbers = string.digits  # Includes digits 0-9
+symbols = string.punctuation  # Includes various punctuation symbols
 
+# Ask the user how many characters of each type they want in the password
 no_of_letters = int(input("\nHow many letters do you want in your password? "))
 no_of_numbers = int(input("How many numbers do you want in your password? "))
 no_of_symbols = int(input("How many symbols do you want in your password? "))
 
-password = ""
+password = ""  # Initialize an empty string to store the password
 
+# Generate the password by randomly selecting characters from each set
 for i in range(1, no_of_letters + 1):
     password += random.choice(letters)
 
@@ -20,8 +23,12 @@ for i in range(1, no_of_numbers + 1):
 for i in range(1, no_of_symbols + 1):
     password += random.choice(symbols)
 
+# Convert the password string to a list and shuffle the characters randomly
 final_password = list(password)
 random.shuffle(final_password)
+
+# Convert the shuffled list back to a string to get the final password
 final_password = "".join(final_password)
 
+# Display the suggested password to the user
 print("\nYour suggested password is: " + final_password)
